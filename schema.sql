85ce545acf21d4a6ac428115ff557d7ee8d7daf3
Jordan <jnbarringer96@gmail.com>
2022-12-06 12:03:56 -0600

    edited the tables for the primary keys and foreign keys

--- schema.sql
+++ schema.sql
@@ -3,21 +3,31 @@ CREATE DATABASE owner_db
 USE owner_db
 
 CREATE TABLE department (
-    id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(30) NOT NULL
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 )
 
 CREATE TABLE role (
-    id INT PRIMARY KEY NOT NULL,
+    id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT FOREIGN KEY NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    PRIMARY KEY (id)
 )
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY NOT NULL,
+    id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL FOREIGN KEY,
-    manager_id INT NOT NULL FOREIGN KEY
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+
 )
